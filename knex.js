246a3292bdf8e3e95d47bd1ada51ee33a8dbff63
Evan Busse <evanbusse@gmail.com>
2017-03-07 15:18:46 -0700

    get projects route working

--- knex.js
+++ knex.js
@@ -1,4 +1,4 @@
 'use strict';
-cont env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require('./knexfile')[env];
 module.exports = require('knex')(config);
--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 
 const express = require('express');
 const app = express();
+const knex = require('./knex');
 
 const bodyParser = require('body-parser');
 
 const port = 5000;
+
+app.get('/projects', (req, res) => {
+  knex('projects').then((data)=>{
+    res.send(data);
+  })
+});
 
 app.listen(port, () => {
   console.log('listening on port', port);
