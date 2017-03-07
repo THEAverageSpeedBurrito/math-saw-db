7f67df8c81c262635298b4c49e9bf56ff68dff4d
Evan Busse <evanbusse@gmail.com>
2017-03-07 15:26:40 -0700

    added cors support

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser');
 const port = 5000;
 
 app.get('/projects/:code', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
   knex('projects')
   .where('code', req.params.code)
   .then((data)=>{
@@ -17,6 +19,8 @@ app.get('/projects/:code', (req, res) => {
 });
 
 app.get('/components/:projectId', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  
   knex('components')
   .where('project', req.params.projectId)
   .then((data) => {
